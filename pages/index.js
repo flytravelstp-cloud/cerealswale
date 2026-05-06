a173654476433472cc16c5055cc786feb08acccd
flytravelstp-cloud <flytravelstp@gmail.com>
2026-05-06 16:23:37 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:'center', padding:'50px'}}>
+      <h1>🌾 Cerealswale</h1>
+      <h2>आधुनिक खेती के लिए, आधुनिक समाधान – सीधे आपके द्वार।</h2>
+      <p>उच्च गुणवत्ता वाले कृषि उत्पाद, जो आपकी फसल और मुनाफे दोनों को बढ़ाएं।</p>
+      <button>अभी खरीदारी करें</button>
+    </div>
+  );
+}
